a78834c95a530e7a3495d2e07f31bea405b1646d
twistzzmc <michal.szczepaniak1999@gmail.com>
2020-10-24 23:37:02 +0200

    Almost finished lab1

---
+++ Lab1/lextab.py
@@ -0,0 +1,10 @@
+# lextab.py. This file automatically created by PLY (version 3.11). Don't edit!
+_tabversion   = '3.10'
+_lextokens    = set(('A_ADD', 'A_DIV', 'A_MUL', 'A_SUB', 'BREAK', 'CONTINUE', 'ELSE', 'EQ', 'EYE', 'FLOAT', 'FOR', 'GE', 'ID', 'IF', 'INT', 'LE', 'M_ADD', 'M_DIV', 'M_MUL', 'M_SUB', 'M_TRANSPOSE', 'NEQ', 'ONES', 'PRINT', 'RETURN', 'STRING', 'WHILE', 'ZEROS'))
+_lexreflags   = 64
+_lexliterals  = '+-*/=<>()[]{}:,;'
+_lexstateinfo = {'INITIAL': 'inclusive'}
+_lexstatere   = {'INITIAL': [('(?P<t_ID>[a-zA-Z_][a-zA-Z_0-9]*)|(?P<t_INT>\\d+)|(?P<t_FLOAT>\\d*((\\d\\.|\\.\\d)\\d*([Ee][+-]?\\d+)?|\\d([Ee][+-]?\\d+)))|(?P<t_STRING>"\\S*")|(?P<t_newline>\\n+)|(?P<t_M_ADD>\\.\\+)|(?P<t_M_MUL>\\.\\*)|(?P<t_ignore_COMMENT>\\#.*)|(?P<t_A_ADD>\\+=)|(?P<t_A_MUL>\\*=)|(?P<t_M_SUB>\\.-)|(?P<t_M_DIV>\\./)|(?P<t_EQ>==)|(?P<t_NEQ>!=)|(?P<t_LE><=)|(?P<t_GE>>=)|(?P<t_A_SUB>-=)|(?P<t_A_DIV>/=)|(?P<t_TRANSPOSE>\\\')', [None, ('t_ID', 'ID'), ('t_INT', 'INT'), ('t_FLOAT', 'FLOAT'), None, None, None, None, ('t_STRING', 'STRING'), ('t_newline', 'newline'), (None, 'M_ADD'), (None, 'M_MUL'), (None, None), (None, 'A_ADD'), (None, 'A_MUL'), (None, 'M_SUB'), (None, 'M_DIV'), (None, 'EQ'), (None, 'NEQ'), (None, 'LE'), (None, 'GE'), (None, 'A_SUB'), (None, 'A_DIV'), (None, 'TRANSPOSE')])]}
+_lexstateignore = {'INITIAL': '  \t'}
+_lexstateerrorf = {'INITIAL': 't_error'}
+_lexstateeoff = {}
---
+++ Lab1/scanner.py
@@ -0,0 +1,76 @@
+import ply.lex as lex
+
+
+literals = ['+', '-', '*', '/', '=', '<', '>', '(', ')', '[', ']', '{', '}',
+            ':', ',', ';']
+
+reserved = {'if': 'IF', 'else': 'ELSE', 'for': 'FOR', 'while': 'WHILE',
+            'break': 'BREAK', 'continue': 'CONTINUE', 'return': 'RETURN',
+            'eye': 'EYE', 'zeros': 'ZEROS', 'ones': 'ONES', 'print': 'PRINT'}
+
+tokens = ['ID', 'EQ', 'NEQ', 'LE', 'GE', 'M_TRANSPOSE', 'M_ADD', 'M_SUB', 'M_MUL',
+          'M_DIV', 'A_ADD', 'A_SUB', 'A_MUL', 'A_DIV', 'INT', 'FLOAT', 'STRING'] \
+         + list(reserved.values())
+
+
+t_EQ = r'=='
+t_NEQ = r'!='
+t_LE = r'<='
+t_GE = r'>='
+
+t_A_ADD = r'\+='
+t_A_SUB = r'-='
+t_A_MUL = r'\*='
+t_A_DIV = r'/='
+
+t_M_ADD = r'\.\+'
+t_M_SUB = r'\.-'
+t_M_MUL = r'\.\*'
+t_M_DIV = r'\./'
+
+t_TRANSPOSE = r'\''
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')    # Check for reserved words
+    return t
+
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_FLOAT(t):
+    r'\d*((\d\.|\.\d)\d*([Ee][+-]?\d+)?|\d([Ee][+-]?\d+))'
+    t.value = float(t.value)
+    return t
+
+
+def t_STRING(t):
+    r'"\S*"'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def find_column(text, token):
+    line_start = text.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+
+t_ignore = '  \t'
+t_ignore_COMMENT = r'\#.*'
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex(optimize=True)
